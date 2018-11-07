47ff4bbeeb0873f9dce7d22ce1491921a2ed6a38
Russel Dsouza <russel.dsouza47@gmail.com>
2018-11-07 16:36:15 +0530

    add required standard header files to IO.h

--- src/IO.h
+++ src/IO.h
@@ -3,16 +3,11 @@
 
 /*http://pubs.opengroup.org/onlinepubs/7908799/xsh/search.h.html*/
 #include<search.h>
-const char delim[] = {".,:;`/\"+-_(){}[]<>*&^%$#@!?~/|\\=1234567890 \t\n"};
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+#include<string.h>
 
-/*
-  Function to read the Dictionary file any add words into a Dictionary
-  Input
-    input_file_name: path to dictionary file
-    dict: structure with char* key and void* data for searching
-  Output
-    0: The dictionary file could not be read
-    1: The dictionary file was read and words were put into a dictionary
-*/
 int readFile(char* input_file_name, ENTRY dict);
+
 #endif
