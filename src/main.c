85d6b044e8bfe1dce0ad483f2d0b289679235f34
Russel Dsouza <russel.dsouza47@gmail.com>
2018-11-07 17:51:13 +0530

    modify main.c

--- src/main.c
+++ src/main.c
@@ -1,6 +1,30 @@
 #include "IO.h"
 
-int main(int argc, char* argv[])
+#define DICT_SIZE 3000000
+
+int main(int argc, char** argv)
 {
+  if(argc != 2)
+  {
+    fprintf(stderr, "Usage: ./spellcheck <word>\n");
+    exit(0);
+  }
+
+  ENTRY dict;
+  hcreate(DICT_SIZE);
+
+  if(!readFile(dict))
+  {
+    fprintf(stderr, "\nCould not read dictionary file: %s\n", DICTIONARY);
+    exit(0);
+  }
+  char* suggestion = "the";
+  //suggestion = correct(argv[1]);
+
+  if (strcmp(suggestion, argv[1]))
+    printf("Did you mean \"%s\"?\n", suggestion);
+  else
+    printf("\"%s\" is correct!\n", argv[1]);
+
   return 0;
 }
